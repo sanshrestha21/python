3fd73cb247ef0c2db7765ea5e629c22dfae62c67
san <sanshrestha21@gmail.com>
2019-01-03 14:58:47 +0545

    add new file

---
+++ algorithm-crude.py
@@ -0,0 +1,24 @@
+text="welcome"
+pattern="welcame"
+
+tlen=len(text)
+plen=len(pattern)
+
+i=0
+
+if(tlen==plen):
+    if(text[i]==pattern[i] and text[plen-1]==pattern[plen-1]):
+        match=True
+        for x in range(1,plen-1):
+            if(text[x]!=pattern[x]):
+                match=False
+        if(match==True):
+            print("match")
+        else:
+            print("not matched")
+    else:
+        print("not matched")
+    
+else:
+    print("not matched")
+    
